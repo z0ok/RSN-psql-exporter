6c43778195a05009002c7e3c5768fbb6a0f5404d
z0ok <zzzoookkk@yandex.ru>
2023-05-20 12:51:14 +0000

    init

---
+++ runner.sh
@@ -0,0 +1,14 @@
+if id 'psql_exporter' &>/dev/null; then
+    echo '[+] User found'
+else
+    echo '[-] Creating user'
+    useradd -rs /bin/false psql_exporter
+fi
+cp ./node_exporter.service /etc/systemd/system/
+systemctl daemon-reload
+sleep 1
+systemctl enable psql_exporter
+sleep 1
+systemctl restart psql_exporter
+sleep 1
+systemctl status psql_exporter
